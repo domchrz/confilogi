483b150951c91e973abe50cd5322505cf24d13a5
myszitsu <d.chrzescijanek@gmail.com>
2023-12-13 14:32:46 +0100

    webpack setup for react.js with sass & other packages for switft build process

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.scss';
+
+export default function App() {
+  return (
+    <div className="app">
+      <header>
+        <h1>Hello App!</h1>
+      </header>
+      <p>Welcome to the app</p>
+    </div>
+  );
+}
